783e9a4a12d18f67c692cf1625bcf70ae91a29cc
Wim Lavrijsen <WLavrijsen@lbl.gov>
2019-11-27 15:41:44 -0800

    add convenience function to list the available methods

--- skquant/opt/__init__.py
+++ skquant/opt/__init__.py
@@ -1,6 +1,31 @@
 from __future__ import print_function
 
+import subprocess
+import sys
 from SQCommon import Result, ObjectiveFunction
+
+
+__all__ = [
+    'methods',
+    'minimize',
+    ]
+
+def _check_orbit_prerequisites():
+    try:
+        import rpy2
+        return True
+    except ImportError:
+        pass
+    return False
+
+def methods():
+    """Returns a list of available optimizer methods"""
+
+    m = ['bobyqa', 'imfil']
+    if _check_orbit_prerequisites():
+        m.append('orbit')
+    m.append('snobfit')
+    return m
 
 
 def minimize(func, x0, bounds, budget=10000, method='imfil', options=None, **optkwds):
@@ -16,8 +41,7 @@ def minimize(func, x0, bounds, budget=10000, method='imfil', options=None, **opt
     elif 'bobyqa' in method_:
         import skquant.opt._pybobyqa as optimizer
     elif 'orbit' in method_:
-        import sys
-        if sys.version_info[0] != 3:
+        if not _check_orbit_prerequisites():
             raise RuntimeError("ORBIT is only supported on Python3 because of rpy2")
         import skquant.opt._norbitR as optimizer
 
