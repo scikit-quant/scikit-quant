c964ae5c8af6b8bc2ea0a9db6657533eeebecdc2
Wim Lavrijsen <WLavrijsen@lbl.gov>
2019-12-03 22:39:31 -0800

    up version

--- opt/imfil/setup.py
+++ opt/imfil/setup.py
@@ -60,6 +60,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
 
         'Natural Language :: English'
     ],
