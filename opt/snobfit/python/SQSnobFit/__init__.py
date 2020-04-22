b584c16aac8ad68636d65d3165cb40b0544e0902
Wim Lavrijsen <WLavrijsen@lbl.gov>
2020-04-21 20:55:16 -0700

    basic logging setup for direct import

--- opt/snobfit/python/SQSnobFit/__init__.py
+++ opt/snobfit/python/SQSnobFit/__init__.py
@@ -2,6 +2,17 @@ from __future__ import print_function
 import logging
 
 log = logging.getLogger('SKQ.SnobFit')
+if not log.hasHandlers():
+    def _setupLogger(log):
+        import sys
+        hdlr = logging.StreamHandler(sys.stdout)
+        frmt = logging.Formatter('%(name)-12s: %(levelname)8s %(message)s')
+        hdlr.setFormatter(frmt)
+        log.addHandler(hdlr)
+        log.propagate = False
+    _setupLogger(log)
+    del _setupLogger
+
 
 log.info("""
 ------------------------------------------------------------------------
