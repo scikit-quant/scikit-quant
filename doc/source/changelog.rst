f899c09fe00fb074b9c18050c97c6202b9688c26
Wim Lavrijsen <WLavrijsen@lbl.gov>
2020-04-30 21:34:55 -0700

    up versions

--- doc/source/changelog.rst
+++ doc/source/changelog.rst
@@ -3,9 +3,14 @@
 Changelog
 =========
 
-
-master: 0.6.0
+master: 0.6.1
 -------------
+
+* Fix indexing error and double delete in NaN handling in SnobFit (Jan Rittig)
+
+
+2020-04-28: 0.6.0
+-----------------
 
 * Remove use of numpy.matrix in SQImFil
 * Start of documentation
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requirements = []
 # optimizers sub-package requirements
 requirements.append('SQCommon==0.3.1')
 requirements.append('SQImFil==0.3.5')
-requirements.append('SQSnobFit==0.4.2')
+requirements.append('SQSnobFit==0.4.3')
 
 # external optimizers
 requirements.append('Py-BOBYQA>=1.1')
