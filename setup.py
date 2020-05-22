d021bd559523a6bdba6bb6a421c5ee5c9247f47e
Wim Lavrijsen <WLavrijsen@lbl.gov>
2020-05-22 16:50:44 -0700

    up requirements

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ requirements = []
 
 # optimizers sub-package requirements
 requirements.append('SQCommon==0.3.1')
-requirements.append('SQImFil==0.3.5')
+requirements.append('SQImFil==0.3.6')
 requirements.append('SQSnobFit==0.4.3')
 
 # external optimizers
